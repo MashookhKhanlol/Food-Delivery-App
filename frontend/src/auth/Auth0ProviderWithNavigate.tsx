eae751d25aa166237124f2b5e0a39c6fb92d6096
Mashookh Khan <mashooq.ahmed175@gmail.com>
2024-12-05 17:25:59 +0530

    checkout changes

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,5 @@
 import { useCreateMyUser } from "@/api/MyUserApi";
-import {  Auth0Provider } from "@auth0/auth0-react";
+import {  AppState, Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -19,8 +19,8 @@ const Auth0ProviderWithNavigate = ({children}: Props) => {
         throw new Error("unable to initialize auth");
     }
 
-    const onRedirectCallback = ()=>{
-        navigate("/auth-callback")
+    const onRedirectCallback = (appState?:AppState)=>{
+        navigate(appState?.returnTo ||"/auth-callback")
         
     }
 
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -7,11 +7,39 @@ export type User={
     country : string
 }
 
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
 export type MenuItem ={
     _id : string;
     name : string;
     price : number
 }
+
+export type Order = {
+    _id: string;
+    restaurant: Restaurant;
+    user: User;
+    cartItems: {
+      menuItemId: string;
+      name: string;
+      quantity: string;
+    }[];
+    deliveryDetails: {
+      name: string;
+      addressLine1: string;
+      city: string;
+      email: string;
+    };
+    totalAmount: number;
+    status: OrderStatus;
+    createdAt: string;
+    restaurantId: string;
+  };
 
 export type Restaurant ={
     _id : string; 
